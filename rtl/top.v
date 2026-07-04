2c4bf9b92e8ac47791e0022cad9bd75c8851fdea
jsponder <jackponder3@gmail.com>
2026-07-04 16:10:52 -0400

    Added basic scoreboard functionality

--- rtl/top.v
+++ rtl/top.v
@@ -1,6 +1,9 @@
 module top
 (
     input clk,
+    input [15:0] sw,
+
+    output [15:0] led,
     output [3:0] vgaRed, 
     output [3:0] vgaGreen, 
     output [3:0] vgaBlue,
@@ -24,11 +27,15 @@ vgaController controller (
     .clk(clk),
     .clkEn(clkEn),
     .rst(rst),
+    .score1(sw[15:12]),
+    .score2(sw[3:0]),
     .hSync(hSync),
     .vSync(vSync),
     .vgaRed(vgaRed),
     .vgaGreen(vgaGreen),
     .vgaBlue(vgaBlue)
 );
+
+assign led = sw;
 
 endmodule
--- rtl/vgaController.v
+++ rtl/vgaController.v
@@ -4,6 +4,9 @@ module vgaController
     input clkEn,
     input rst,
 
+    input [3:0] score1,
+    input [3:0] score2,
+
     output hSync,
     output vSync,
     output [3:0] vgaRed,
@@ -50,24 +53,52 @@ blockRom #(
     .data(bgPixel)
 );
 
+localparam SCORE_WIDTH = 50;
+localparam SCORE_HEIGHT = 60;
+
 localparam SCORE_1_X_START = 255;
-localparam SCORE_1_X_END = SCORE_1_X_START + 50;
+localparam SCORE_1_X_END = SCORE_1_X_START + SCORE_WIDTH;
 localparam SCORE_1_Y_START = 125;
-localparam SCORE_1_Y_END = SCORE_1_Y_START + 60;
+localparam SCORE_1_Y_END = SCORE_1_Y_START + SCORE_HEIGHT;
 
 localparam SCORE_2_X_START = 335;
-localparam SCORE_2_X_END = SCORE_2_X_START + 50;
+localparam SCORE_2_X_END = SCORE_2_X_START + SCORE_WIDTH;
 localparam SCORE_2_Y_START = 125;
-localparam SCORE_2_Y_END = SCORE_2_Y_START + 60;
+localparam SCORE_2_Y_END = SCORE_2_Y_START + SCORE_HEIGHT;
 
 wire activeScore1, activeScore2;
 assign activeScore1 = (SCORE_1_X_START <= x) && (x < SCORE_1_X_END) && (SCORE_1_Y_START <= y) && (y < SCORE_1_Y_END);
 assign activeScore2 = (SCORE_2_X_START <= x) && (x < SCORE_2_X_END) && (SCORE_2_Y_START <= y) && (y < SCORE_2_Y_END);
 
+localparam DIGIT_PIXEL_COUNT = SCORE_WIDTH * SCORE_HEIGHT;
+localparam DIGITS_PIXEL_COUNT = DIGIT_PIXEL_COUNT * 10;
+
+wire [$clog2(DIGITS_PIXEL_COUNT)-1:0] numberAddr;
+wire numberPixel;
+
+assign numberAddr = ( 
+    activeScore1 ? (
+        (x - SCORE_1_X_START) + (SCORE_WIDTH * (y - SCORE_1_Y_START)) + (DIGIT_PIXEL_COUNT * score1)
+    ) : activeScore2 ? (
+        (x - SCORE_2_X_START) + (SCORE_WIDTH * (y - SCORE_2_Y_START)) + (DIGIT_PIXEL_COUNT * score2)
+    ) : 0
+);
+
+blockRom #(
+    .DEPTH(DIGITS_PIXEL_COUNT),
+    .WIDTH(1),
+    .INIT_FILE("digits.mem")
+) numbersData (
+    .clk(clk),
+    .clkEn(clkEn),
+    .addr(numberAddr),
+    .data(numberPixel)
+);
+
 wire [11:0] colorOut;
 assign colorOut = active ? (
-    activeScore1 ? 12'hF00 :
-    activeScore2 ? 12'h0FF :
+    activeScore1 ? (numberPixel ? 12'hFFF : 0) :
+    activeScore2 ? (numberPixel ? 12'hFFF : 0) :
     bgPixel ? 12'hFFF : 0 
 ) : 0;
 
